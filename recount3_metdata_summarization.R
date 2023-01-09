9f6bf16e27eb21e3dff427c53b0649986dcee1b5
lagillenwater <lagillenwater@gmail.com>
2023-01-09 11:34:34 -0700

    Added function for isolating the rows of the metadata with variable of interest

--- recount3_metdata_summarization.R
+++ recount3_metdata_summarization.R
@@ -1,12 +1,32 @@
 ### This is a script for reading in the metadata preprocessed in the recount3_tissue.R. That script took all the metadata files and extracted the main variable of interest: "sra.sample_attributes". The txt files containing that information is located in the directory recount3_metadata.
 
 
-library(tidyverse)
 
 ## readMetadata is a script for reading in the text metadata file. It takes as an input the metadata file.
 readMetadata <- function(metadata_file) {
     metadata <- read.delim(metadata_file, header = F)
+    return(metadata)
 }
 
 
-## 
+## readMetadataWrapper is a function for applying the metadata wrapper over all the files in the recount3_metadata directory.
+readMetadataWrapper <- function(metadata_dir) {
+    metadata_files <- paste0(metadata_dir, list.files(metadata_dir))
+    metadata <- lapply(metadata_files, function(x) {
+        tryCatch({readMetadata(x)}, error = function(e) {})})      
+    return(metadata)
+}
+
+
+##### NB: could improe readMetadataWrapper or readMetadata by converting the text file to a vectore instead of a data frame. Would save space
+
+## 7510 studies have metadata
+
+## findVariable is a function for finding the rows that contain the keyword of interest in the metadata tables.
+## Takes as an input the metadata table to search and the variable of interest. 
+findVariable <- function(metadata_table, variable) {
+    metadata <- metadata_table$V1[grepl(variable, metadata_table$V1)]
+    return(metadata)
+}
+
+## findVariableWrapper is a wrapper function for applying the findVariable over a list of metadata data frames. 
