efd194b6bfa28d5c7c0fee301a03b03e33b4f76a
lagillenwater <lagillenwater@gmail.com>
2023-04-29 11:47:44 -0600

    removed tutorial code

--- src/ssGSEA_evaluation.R
+++ src/ssGSEA_evaluation.R
@@ -13,41 +13,12 @@ devtools::install_github("nicolerg/ssGSEA2")
 library(ssGSEA2)
 library(tidyverse)
 library(cmapR)
+library(ComplexHeatmap)
 
 ## Load the data
-load("../data/HTP_transcription_counts_wide_1000.Rdata")
+load("../data/HTP_transcription_counts_wide_protein_coding_variance_filtered.Rdata")
 
 htp_expr <- expression_list$expression
-
-# Download example input
-download.file(url = "https://raw.githubusercontent.com/nicolerg/ssGSEA2/master/example/PI3K_pert_logP_n2x23936.gct",              destfile = "/tmp/PI3K_pert_logP_n2x23936.gct")
-
-# Download gene set database 
-download.file(url = "https://raw.githubusercontent.com/nicolerg/ssGSEA2/master/example/ptm.sig.db.all.flanking.human.v1.8.1.gmt",destfile  = "/tmp/ptm.sig.db.all.flanking.human.v1.8.1.gmt")
-
-res = run_ssGSEA2("/tmp/PI3K_pert_logP_n2x23936.gct",
-                  output.prefix = "example",
-                  gene.set.databases = "/tmp/ptm.sig.db.all.flanking.human.v1.8.1.gmt",
-                  output.directory = "/tmp",
-                  sample.norm.type = "none", 
-                  weight = 0.75, 
-                  correl.type = "rank", 
-                  statistic = "area.under.RES",
-                  output.score.type = "NES", 
-                  nperm = 1000, 
-                  min.overlap = 5, 
-                  extended.output = TRUE, 
-                  global.fdr = FALSE,
-                  log.file = "/tmp/run.log")
-
-
-
-## cmap exploration
-
-
-my_ds <- parse_gctx("/tmp/PI3K_pert_logP_n2x23936.gct")
-
-
 
 
 ### Testing testing with data
@@ -60,13 +31,13 @@ htp_gct_mat <- htp_expr %>%
 htp_gct <- new("GCT", htp_gct_mat)
 
 ## writing gct object to disk
-write_gct(htp_gct, "../data/HTP_transcription_counts.gct", appenddim = F)
+write_gct(htp_gct, "../data/HTP_transcription_counts_protein_coding_variance_filtered.gct", appenddim = F)
 
 ## Hallmark Gene Sets
 
 ## testing
 
-res = run_ssGSEA2("../data/HTP_transcription_counts.gct",
+res = run_ssGSEA2("../data/HTP_transcription_counts_protein_coding_variance_filtered.gct",
                   output.prefix = "test_hallmarks",
                   gene.set.databases = "../data/Gene Pathways/h.all.v2023.1.Hs.symbols.gmt",
                   output.directory = "../results/tests",
@@ -75,8 +46,35 @@ res = run_ssGSEA2("../data/HTP_transcription_counts.gct",
                   correl.type = "rank", 
                   statistic = "area.under.RES",
                   output.score.type = "NES", 
-                  nperm = 1000, 
+                  nperm = 10, 
                   min.overlap = 5, 
                   extended.output = TRUE, 
                   global.fdr = FALSE,
                   log.file = "../results/tests/run.log")
+
+scores <- parse_gctx("../results/tests/test_hallmarks-scores.gct")
+scores_mat <- mat(scores)
+
+### mapping the data
+
+### loading metadata 
+meta <- read.delim("../data/P4C_metadata_021921_Costello.txt")
+
+### processing metadata
+meta_heatmap <- meta %>%
+    select(LabID, Karyotype) %>%
+    filter(LabID %in% ids(scores, dimension = "column")) %>%
+    arrange(match(LabID, ids(scores, dimension = "column"))) %>%
+    column_to_rownames('LabID')
+
+## fitlering scores matrix
+scores_mat <- scores_mat[, colnames(scores_mat) %in% rownames(meta_heatmap)]
+
+identical(rownames(meta_heatmap), colnames(scores_mat))
+
+ha <- HeatmapAnnotation(df = meta_heatmap)
+
+Heatmap(scores_mat,
+        show_column_names = F,
+        top_annotation = ha,
+        row_names_gp = grid::gpar(fontsize = 7))
