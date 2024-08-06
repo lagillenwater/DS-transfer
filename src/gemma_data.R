ecaf22d5263362597757e1d02331108ca32a3379
lagillenwater <lagillenwater@gmail.com>
2024-08-06 12:04:53 -0600

    formatting

--- src/gemma_data.R
+++ src/gemma_data.R
@@ -2,7 +2,7 @@
 ##libraries
 suppressMessages(library(dplyr))
 
-## load DS data
+
 ## load(file = "./data/GEMMA/DS_experiments.Rdata")
 ## load(file = "./data/GEMMA/DS_expression.Rdata")
 ## load(file = "./data/GEMMA/DS_samples.Rdata")
@@ -15,8 +15,9 @@ suppressMessages(library(dplyr))
 ## AML_samples[4,"sample.FactorValues" ][[1]]
 ## AML_samples[1,]
 
-load(file = "./data/GEMMA/PBMC_experiments.Rdata")
-load(file = "./data/GEMMA/PBMC_expression.Rdata")
-load(file = "./data/GEMMA/PBMC_samples.Rdata")
-PBMC_samples[,"sample.Characteristics" ][[1]]
-PBMC_samples[,"sample.FactorValues" ][[1]]
+##load(file = "./data/GEMMA/PBMC_experiments.Rdata")
+
+## load(file = "./data/GEMMA/PBMC_expression.Rdata")
+## load(file = "./data/GEMMA/PBMC_samples.Rdata")
+## PBMC_samples[,"sample.Characteristics" ][[1]]
+## PBMC_samples[,"sample.FactorValues" ][[1]]
