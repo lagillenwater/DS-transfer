f9a242401d4553768b41ea81380cb093ca28ba2e
lagillenwater <lagillenwater@gmail.com>
2024-08-06 11:35:39 -0600

    script for exploring GEMMA data

---
+++ src/gemma_data.R
@@ -0,0 +1,22 @@
+
+##libraries
+suppressMessages(library(dplyr))
+
+## load DS data
+## load(file = "./data/GEMMA/DS_experiments.Rdata")
+## load(file = "./data/GEMMA/DS_expression.Rdata")
+## load(file = "./data/GEMMA/DS_samples.Rdata")
+## DS_samples[4,"sample.Characteristics" ][[1]]
+
+## load(file = "./data/GEMMA/AML_experiments.Rdata")
+## load(file = "./data/GEMMA/AML_expression.Rdata")
+## load(file = "./data/GEMMA/AML_samples.Rdata")
+## AML_samples[4,"sample.Characteristics" ][[1]]
+## AML_samples[4,"sample.FactorValues" ][[1]]
+## AML_samples[1,]
+
+load(file = "./data/GEMMA/PBMC_experiments.Rdata")
+load(file = "./data/GEMMA/PBMC_expression.Rdata")
+load(file = "./data/GEMMA/PBMC_samples.Rdata")
+PBMC_samples[,"sample.Characteristics" ][[1]]
+PBMC_samples[,"sample.FactorValues" ][[1]]
